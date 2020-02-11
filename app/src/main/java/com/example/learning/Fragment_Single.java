fa953f6913520a907d55590f970ea625b7ec45ba
annu1794 <anuragshukla9471@gmail.com>
2020-02-11 09:58:01 +0530

    initial

---
+++ app/src/main/java/com/example/learning/Fragment_Single.java
@@ -0,0 +1,15 @@
+package com.example.learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment_Single extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment__single);
+
+    }
+}
